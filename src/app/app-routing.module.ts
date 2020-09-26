ad11366140d65c9b855d8c06f0a1e389f12a36aa
System Administrator <reza.razjoo@yahoo.com>
2020-09-26 17:59:31 +0100

    added address component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
   {path: '', component: MainComponent},
-  { path: 'address', loadChildren: () => import('./address/address.module').then(m => m.AddressModule) }
+  { path: 'address', loadChildren: () => import('./address/address.module').then(m => m.AddressModule) },
+  { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) }
 ];
 
 @NgModule({
