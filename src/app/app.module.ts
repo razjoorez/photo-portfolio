e7c4076c75d51e56ced056cc972ee97cbc348e13
System Administrator <root@rezas-MacBook-Pro.local>
2020-09-27 19:31:15 +0100

    services and data for address

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { MainComponent } from './main/main.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    MainComponent
+    MainComponent,
+    HttpClientModule
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
   ],
+  exports: [
+    HttpClientModule
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
