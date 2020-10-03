86a214b723eb4f73ccf9158d6f7e9897c230555e
System Administrator <reza.razjoo@yahoo.com>
2020-10-04 00:00:40 +0100

    new photos

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -10,14 +10,35 @@ export class RegisterComponent implements OnInit {
   RegisterForm: FormGroup;
   constructor(private fb: FormBuilder) { 
     this.RegisterForm = this.fb.group({
-      email: ['',[ Validators.required, Validators.email]],
+      firstName: [''],
+      lastName: [''],
+      email: ['', Validators.required],
       password: {value: '', disabled: true}
 
     });
   }
 
   ngOnInit() {
+    const emailVal = this.RegisterForm.get('email');
+   // emailVal.clearValidators();
 
+  }
+
+  notifyBy(method: string) {
+    let emailVal = this.RegisterForm.get('email');
+    emailVal.clearValidators();
+    
+    if(method=='email'){
+     
+      //emailVal.setValidators(Validators.required);
+      emailVal.clearValidators();
+     
+      alert(method);
+    }else {
+     // emailVal.clearValidators();
+      alert('cleard')
+    }
+    
   }
 
 }
