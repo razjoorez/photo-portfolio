cb50e8e22461f6b06ee13e4d8608d95e3086c9a0
razjoorez <reza.razjoo@yahoo.com>
2021-03-22 21:31:00 +0000

    moved faker from databse

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IEcommerce } from '../models/IEcommerce';
+import * as faker from 'faker';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
-
+  products = [];
   private REST_API_SERVER = "http://localhost:3001/apis";
-constructor(private http: HttpClient ) { 
-    
+constructor(private http: HttpClient ) {
+
   }
   getProducts(){
-    return this.http.get(this.REST_API_SERVER);
+
+    for(let i=1; i<=301; i++) {
+      this.products.push( {
+        id: i,
+        name: faker.commerce.productName(),
+        description: faker.lorem.sentences(),
+        price: faker.commerce.price(),
+        imageUrl: "https://source.unsplash.com"
+      });
+    }
+   // return this.http.get(this.REST_API_SERVER);
+   return of(this.products);
   }
 }
